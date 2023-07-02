2dd92905af5a18440a9484304012c2e708f5b722
Dovilė Skiriūtė <dsk@intersurgical.lt>
2023-07-02 14:58:41 +0300

    Create component

--- weather-forecast/src/app/app.module.ts
+++ weather-forecast/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WeatherForecastComponent } from './weather-forecast/weather-forecast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherForecastComponent
   ],
   imports: [
     BrowserModule
